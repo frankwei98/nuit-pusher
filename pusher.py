5ef36967919d8331625a0f69e364c04b9b449833
Frank Wei <frank@frankwei.xyz>
2017-10-15 23:55:24 +0800

    Fix bug for test

--- pusher.py
+++ pusher.py
@@ -30,8 +30,7 @@ def push(key=config.DEFAULT_SENDKEY, title='Empty Title', message=None):
 def push_daily():
     push(
         title='每日课表推送',
-        # message=search.get_today_msg()
-        message=search.get_msg_of_the_day(5, 1)
+        message=search.get_today_msg()
     )
 
 
